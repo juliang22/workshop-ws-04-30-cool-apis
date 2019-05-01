f425a2edd1ac2b0f93e662d0e65e8a9d48b87ff0
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2019-05-01 14:22:29 -0400

    finished yesterday, forgot to update

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Recorder from './components/Recorder';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false,
+      // eslint-disable-next-line react/no-unused-state
+      audioText: 'click the microphone to record some audio!',
+      // eslint-disable-next-line react/no-unused-state
+      audioBlob: null,
+    };
   }
 
-  render() {
-    return (
-      <div />
-    );
+  handleLoading = () => {
+    if (this.state.loading) {
+      return <CircularProgress color="secondary" />;
+    }
+    return <Recorder microphoneStarted={this.microphoneStarted} sendAudioBlob={this.getAudioBlob} />;
   }
+
+microphoneStarted = () => {
+  this.setState({
+    // eslint-disable-next-line react/no-unused-state
+    audioText: 'listening...',
+  });
+}
+
+getAudioBlob = (blob) => {
+  this.setState({
+    // eslint-disable-next-line react/no-unused-state
+    audioBlob: blob,
+  });
+  this.sendRequest();
+}
+
+sendRequest = () => {
+  console.log('sending a request to IBM...');
+}
+
+render() {
+  return (
+    <div className="container">
+      {this.handleLoading()}
+    </div>
+  );
+}
 }
 
 ReactDOM.render(<App />, document.getElementById('main'));
